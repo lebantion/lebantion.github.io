0ed25ba74e0c67292514905c5e3233fbbf7b1524
lebantion <58497274+lebantion@users.noreply.github.com>
2023-05-09 17:41:16 -0400

    file path

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './home.css';
+import './Home.css';
 import Aos from "aos";
 import "aos/dist/aos.css";
 
--- src/pages/Projects/Project.jsx
+++ src/pages/Projects/Project.jsx
@@ -1,7 +1,7 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import './project.css';
+import './Project.css';
 import logo from '../../assets/project.png'
 
 import Aos from "aos";
