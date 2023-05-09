f075f5b0224f8d17168b5a4a1ed1226ca4f5020d
lebantion <58497274+lebantion@users.noreply.github.com>
2023-05-09 17:47:15 -0400

    change path

--- src/pages/Projects/Project.jsx
+++ src/pages/Projects/Project.jsx
@@ -1,7 +1,7 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import './Project.css';
+import './project.css';
 import logo from '../../assets/project.png'
 
 import Aos from "aos";
