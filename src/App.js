a9fcf61540e26d328257e4205cf60a45b2d89fb9
lebantion <58497274+lebantion@users.noreply.github.com>
2023-05-09 17:45:35 -0400

    trying to fix error

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 import Footer from "./Footer";
 
-import Homepage from "./pages/home/Home";
-import Diplomapage from "./pages/diploma/Diploma";
-import Projectpage from "./pages/projects/Project";
-import Contactpage from "./pages/contact/Contact";
+import Homepage from "./pages/Home/home";
+import Diplomapage from "./pages/Diploma/diploma";
+import Projectpage from "./pages/Projects/project";
+import Contactpage from "./pages/Contact/contact";
 
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './Home.css';
+import './home.css';
 import Aos from "aos";
 import "aos/dist/aos.css";
 
